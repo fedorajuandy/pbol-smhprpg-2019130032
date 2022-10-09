e4b00bf120bbbbacd67bcecd8288bbfec7545358
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-10-10 00:53:42 +0700

    recreated project (wrong selected), README modified

---
+++ README.md
@@ -0,0 +1,26 @@
+# PBOL
+
+2019130032 - Fedora Yoshe Juandy
+
+## Sistem Maintenance Hero Permainan RPG
+
+DISCLAIMER: every name and content from this application are fictive. Whatever you found from an instance, a human's name, an item, to everything else are just merely (perhaps not so happy) unintended coincidences. Do not ask the idea where those stuff came from because my sleep-derived brain of constant insomnia and paranoia made this whatever possible. No, coffee does not work and I would prefer not to waste more time chasing something impossible at the current moment.
+
+## Codes references
+
+- PBO notes and past projects
+- PGBD past project
+- PBOL notes and past projects
+- <a href=""></a>
+- 
+
+## Ideas references
+
+- Final Fantasy Tactics Advance GBA
+- Elona Mobile
+- 
+- 
+
+## Images references
+
+- 
---
+++ src/pbol/smhprpg/pkg2019130032/Koneksi.java
@@ -0,0 +1,44 @@
+package pbol.smhprpg.pkg2019130032;
+
+import java.sql.*;
+
+/**
+ *
+ * @author 2019130032 - Fedora Yoshe Juandy
+ */
+public class Koneksi {
+    public Connection dbKoneksi;
+    public Statement statement;
+    public PreparedStatement preparedStatement;
+    
+    public Koneksi() {
+        this.dbKoneksi = null;
+    }
+    
+    public void bukaKoneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            dbKoneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_pbol?user=root&password=");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void tutupKoneksi() {
+        try {
+            if (statement != null) {
+                statement.close();
+             }
+            
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            
+            if (dbKoneksi != null) {
+                dbKoneksi.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
