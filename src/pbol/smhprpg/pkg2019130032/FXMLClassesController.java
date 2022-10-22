c196902f6329b93104db05c89a048dd56974caba
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-10-22 14:14:26 +0700

    details view added

--- src/pbol/smhprpg/pkg2019130032/FXMLClassesController.java
+++ src/pbol/smhprpg/pkg2019130032/FXMLClassesController.java
@@ -2,7 +2,13 @@ package pbol.smhprpg.pkg2019130032;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -11,6 +17,21 @@ import javafx.fxml.Initializable;
  */
 public class FXMLClassesController implements Initializable {
 
+    @FXML
+    private TextField search;
+    @FXML
+    private TableView<?> tbv;
+    @FXML
+    private TableView<?> tbvd2;
+    @FXML
+    private TableView<?> tbvd4;
+    @FXML
+    private TableView<?> tbvd1;
+    @FXML
+    private TableView<?> tbvd21;
+    @FXML
+    private TableView<?> tbvd11;
+
     /**
      * Initializes the controller class.
      */
@@ -18,5 +39,121 @@ public class FXMLClassesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void searchData(KeyEvent event) {
+    }
+
+    @FXML
+    private void showDetails(MouseEvent event) {
+    }
+
+    @FXML
+    private void firstClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void addClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void firstDClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevDClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastBtbClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void firstDClicked1(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevDClicked1(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked1(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastBtbClicked1(ActionEvent event) {
+    }
+
+    @FXML
+    private void firstDClicked2(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevDClicked2(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked2(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastBtbClicked2(ActionEvent event) {
+    }
+
+    @FXML
+    private void firstDClicked3(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevDClicked3(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked3(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastBtbClicked3(ActionEvent event) {
+    }
+
+    @FXML
+    private void firstDClicked4(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevDClicked4(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked4(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastBtbClicked4(ActionEvent event) {
+    }
     
 }
