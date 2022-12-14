ef88b73af13aea02e382e4daaaa82d43406ceb4c
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-14 20:44:11 +0700

    add simulations

---
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLSimulationBattlesController.java
@@ -0,0 +1,19 @@
+package pbol.smhprpg.pkg2019130032.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 2019130032 - Fedora Yoshe Juandy
+ */
+public class FXMLSimulationBattlesController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
