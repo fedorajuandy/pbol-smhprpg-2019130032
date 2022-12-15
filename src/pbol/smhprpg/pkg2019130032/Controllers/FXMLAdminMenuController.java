1c698b1831a0761dcf8a3dd2a54e04ece5b3b3be
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-16 04:07:13 +0700

    add admin/user menu

--- src/pbol/smhprpg/pkg2019130032/Controllers/FXMLAdminMenuController.java
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLAdminMenuController.java
@@ -1,28 +1,6 @@
 package pbol.smhprpg.pkg2019130032.Controllers;
 
 import java.io.IOException;
-import pbol.smhprpg.pkg2019130032.DBs.DBBaseStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBBaseToBattleStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBBattleStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBClassBaseStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBClassBattleStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBClassSkills;
-import pbol.smhprpg.pkg2019130032.DBs.DBClassTraits;
-import pbol.smhprpg.pkg2019130032.DBs.DBClasses;
-import pbol.smhprpg.pkg2019130032.DBs.DBEffects;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroBaseStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroClasses;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroEffects;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroSkills;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroTraits;
-import pbol.smhprpg.pkg2019130032.DBs.DBHeroes;
-import pbol.smhprpg.pkg2019130032.DBs.DBRaceBaseStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBRaceTraits;
-import pbol.smhprpg.pkg2019130032.DBs.DBRaces;
-import pbol.smhprpg.pkg2019130032.DBs.DBSkills;
-import pbol.smhprpg.pkg2019130032.DBs.DBTraitBaseStats;
-import pbol.smhprpg.pkg2019130032.DBs.DBTraits;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -40,27 +18,6 @@ import javafx.stage.Stage;
  * @author 2019130032 - Fedora Yoshe Juandy
  */
 public class FXMLAdminMenuController implements Initializable {
-    public static DBBaseStats dtbs = new DBBaseStats();
-    public static DBBaseToBattleStats dtbtb = new DBBaseToBattleStats();
-    public static DBBattleStats dtbt = new DBBattleStats();
-    public static DBClassBaseStats dtcbs = new DBClassBaseStats();
-    public static DBClassBattleStats dtcbt = new DBClassBattleStats();
-    public static DBClassSkills dtcs = new DBClassSkills();
-    public static DBClassTraits dtct = new DBClassTraits();
-    public static DBClasses dtc = new DBClasses();
-    public static DBEffects dte = new DBEffects();
-    public static DBHeroBaseStats dthbs = new DBHeroBaseStats();
-    public static DBHeroClasses dthc = new DBHeroClasses();
-    public static DBHeroEffects dthe = new DBHeroEffects();
-    public static DBHeroSkills dths = new DBHeroSkills();
-    public static DBHeroTraits dtht = new DBHeroTraits();
-    public static DBHeroes dth = new DBHeroes();
-    public static DBRaceBaseStats dtrbs = new DBRaceBaseStats();
-    public static DBRaceTraits dtrt = new DBRaceTraits();
-    public static DBRaces dtr = new DBRaces();
-    public static DBSkills dts = new DBSkills();
-    public static DBTraitBaseStats dttbs = new DBTraitBaseStats();
-    public static DBTraits dtt = new DBTraits();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
---
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLUserMenuController.java
@@ -0,0 +1,22 @@
+package pbol.smhprpg.pkg2019130032.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 2019130032 - Fedora Yoshe Juandy
+ */
+public class FXMLUserMenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
