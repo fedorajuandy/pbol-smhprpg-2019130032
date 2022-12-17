37826a8f078a7a4096d195b5e35127c90588235c
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-18 04:44:22 +0700

    change stuff

--- src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroesSimulationController.java
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroesSimulationController.java
@@ -2,7 +2,11 @@ package pbol.smhprpg.pkg2019130032.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -11,9 +15,28 @@ import javafx.fxml.Initializable;
  */
 public class FXMLHeroesSimulationController implements Initializable {
 
+    @FXML
+    private ImageView imgHero;
+    @FXML
+    private Label lbNameHero;
+    @FXML
+    private Label lbNameHero1;
+    @FXML
+    private Label lbNameHero2;
+    @FXML
+    private Label lbNameHero21;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void prevDClicked1(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextDClicked1(ActionEvent event) {
+    }
     
 }
