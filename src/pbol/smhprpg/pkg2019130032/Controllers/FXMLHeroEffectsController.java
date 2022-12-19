a3acd5d129137bde79169b8e0975db61007884cf
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-19 22:06:26 +0700

    initials

--- src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroEffectsController.java
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroEffectsController.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package pbol.smhprpg.pkg2019130032.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,12 +18,98 @@ import javafx.fxml.Initializable;
  */
 public class FXMLHeroEffectsController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField search1;
+    @FXML
+    private TableView<HeroModel> tbv;
+    @FXML
+    private TableView<EffectModel> tbv1;
+    @FXML
+    private TextField search;
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtId1;
+    @FXML
+    private TextField txtInput;
+    @FXML
+    private Button btnExit;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void searchData1(KeyEvent event) {
+    }
+
+    @FXML
+    private void select(MouseEvent event) {
+    }
+
+    @FXML
+    private void select1(MouseEvent event) {
+    }
+
+    @FXML
+    private void firstClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void prevClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void lastClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void first1Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void prev1Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void next1Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void last1Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void clear2Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void searchData(KeyEvent event) {
+    }
+
+    @FXML
+    private void clear1Clicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void validateInput(KeyEvent event) {
+    }
+
+    @FXML
+    private void saveClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void clearClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitClicked(ActionEvent event) {
+    }
     
 }
