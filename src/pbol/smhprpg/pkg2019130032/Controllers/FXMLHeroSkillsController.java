7ac43c45fcb1d591ad7d54b53f6c7b9e4105570f
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-19 15:30:37 +0700

    add heroes & classes details

---
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroSkillsController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package pbol.smhprpg.pkg2019130032.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 2019130032 - Fedora Yoshe Juandy
+ */
+public class FXMLHeroSkillsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
