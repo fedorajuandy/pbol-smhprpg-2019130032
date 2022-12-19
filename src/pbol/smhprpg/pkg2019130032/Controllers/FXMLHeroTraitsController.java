f1af15e2b858be0d9844b09fea610ffcaa36fe76
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-20 02:04:14 +0700

    finish HeroTraits

--- src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroTraitsController.java
+++ src/pbol/smhprpg/pkg2019130032/Controllers/FXMLHeroTraitsController.java
@@ -2,14 +2,23 @@ package pbol.smhprpg.pkg2019130032.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import pbol.smhprpg.pkg2019130032.Models.HeroTraitModel;
+import pbol.smhprpg.pkg2019130032.Models.HeroModel;
+import pbol.smhprpg.pkg2019130032.Models.HeroTraitModel;
+import pbol.smhprpg.pkg2019130032.Models.TraitModel;
 
 /**
  * FXML Controller class
@@ -17,6 +26,8 @@ import javafx.scene.input.MouseEvent;
  * @author 2019130032 - Fedora Yoshe Juandy
  */
 public class FXMLHeroTraitsController implements Initializable {
+    private boolean edited = false;
+    private int id, id1;
 
     @FXML
     private TextField search1;
@@ -35,71 +46,346 @@ public class FXMLHeroTraitsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        showData();
+        showData1();
+    }
+    
+    public void execute(HeroTraitModel d) {
+        id = d.getHero_id();
+        id1 = d.getTrait_id();
+          
+        if (id != 0 && id1 != 0) {
+          edited = true;
+          
+          for (int i = 0; i < tbv.getItems().size(); i++) {
+              tbv.getSelectionModel().select(i);
+              if (tbv.getSelectionModel().getSelectedItem().getId() == id) {
+                  break;
+              }
+          }
+          for (int i = 0; i < tbv1.getItems().size(); i++) {
+              tbv1.getSelectionModel().select(i);
+              if (tbv1.getSelectionModel().getSelectedItem().getId() == id1) {
+                  break;
+              }
+          }
+          tbv.setSelectionModel(null);
+          tbv1.setSelectionModel(null);
+          
+          txtId.setText(Integer.toString(id));
+          txtId1.setText(Integer.toString(id1));
+        }
+    }
+    
+    public void showData() {
+        ObservableList<HeroModel> data = FXMLMainMenuController.dth.load();
+        
+        if (data != null) {
+            tbv.getColumns().clear();
+            tbv.getItems().clear();
+            
+            TableColumn col = new TableColumn("Id");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("id"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Name");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("name"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Gender");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("gender"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Description");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("des"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Race Id");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("race_id"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Race name");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("raceName"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Curr class id");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("raceName"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Experience");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("exp"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Curr class name");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("className"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Stat points");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("stat_points"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Skill points");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("skill_points"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("User id");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("user_id"));
+            tbv.getColumns().addAll(col);
+            
+            col = new TableColumn("Username");
+            col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("username"));
+            tbv.getColumns().addAll(col);
+            
+            tbv.setItems(data);
+         } else {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Empty data", ButtonType.OK);
+            a.showAndWait();
+            tbv.getScene().getWindow().hide();;
+        }
+    }
+    
+    public void showData1() {
+        ObservableList<TraitModel> data = FXMLMainMenuController.dtt.load();
+        
+        if (data != null) {
+            tbv1.getColumns().clear();
+            tbv1.getItems().clear();
+            
+            TableColumn col = new TableColumn("Id");
+            col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("id"));
+            tbv1.getColumns().addAll(col);
+            
+            col = new TableColumn("Name");
+            col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("name"));
+            tbv1.getColumns().addAll(col);
+            
+            col = new TableColumn("Des");
+            col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("des"));
+            tbv1.getColumns().addAll(col);
+            
+            tbv1.setItems(data);
+         } else {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Empty data", ButtonType.OK);
+            a.showAndWait();
+            tbv1.getScene().getWindow().hide();;
+        }
+    }
 
     @FXML
     private void searchData1(KeyEvent event) {
+        TraitModel s = new TraitModel();
+        String key = search1.getText();
+        
+        if (key != "") {
+            ObservableList<TraitModel> data = FXMLMainMenuController.dtt.searchItems(key, key, key);
+            if (data != null) {
+                tbv1.getColumns().clear();
+                tbv1.getItems().clear();
+                
+                TableColumn col = new TableColumn("Id");
+                col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("id"));
+                tbv1.getColumns().addAll(col);
+
+                col = new TableColumn("Name");
+                col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("name"));
+                tbv1.getColumns().addAll(col);
+
+                col = new TableColumn("Des");
+                col.setCellValueFactory(new PropertyValueFactory<TraitModel, String>("des"));
+                tbv1.getColumns().addAll(col);
+                
+                tbv1.setItems(data);
+                tbv1.getSelectionModel().selectFirst();
+            } else {
+                Alert a = new Alert(Alert.AlertType.ERROR, "Empty data", ButtonType.OK);
+                a.showAndWait();
+                tbv1.getScene().getWindow().hide();;
+            }            
+        } else {
+           showData1();
+        }
     }
 
     @FXML
     private void select(MouseEvent event) {
+        txtId.setText(Integer.toString(tbv.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void select1(MouseEvent event) {
+        txtId1.setText(Integer.toString(tbv1.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void firstClicked(ActionEvent event) {
+        tbv.getSelectionModel().selectFirst();
+        tbv.requestFocus();
+        txtId.setText(Integer.toString(tbv.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void prevClicked(ActionEvent event) {
+        tbv.getSelectionModel().selectAboveCell();
+        tbv.requestFocus();
+        txtId.setText(Integer.toString(tbv.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void nextClicked(ActionEvent event) {
+        tbv.getSelectionModel().selectBelowCell();
+        tbv.requestFocus();
+        txtId.setText(Integer.toString(tbv.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void lastClicked(ActionEvent event) {
+        tbv.getSelectionModel().selectLast();
+        tbv.requestFocus();
+        txtId.setText(Integer.toString(tbv.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void first1Clicked(ActionEvent event) {
+        tbv1.getSelectionModel().selectFirst();
+        tbv1.requestFocus();
+        txtId1.setText(Integer.toString(tbv1.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void prev1Clicked(ActionEvent event) {
+        tbv1.getSelectionModel().selectAboveCell();
+        tbv1.requestFocus();
+        txtId1.setText(Integer.toString(tbv1.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void next1Clicked(ActionEvent event) {
+        tbv1.getSelectionModel().selectBelowCell();
+        tbv1.requestFocus();
+        txtId1.setText(Integer.toString(tbv1.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void last1Clicked(ActionEvent event) {
+        tbv1.getSelectionModel().selectLast();
+        tbv1.requestFocus();
+        txtId1.setText(Integer.toString(tbv1.getSelectionModel().getSelectedItem().getId()));
     }
 
     @FXML
     private void clear2Clicked(ActionEvent event) {
+        search1.setText("");
+        txtId1.setText("");
+        showData1();
+        search1.requestFocus();
     }
 
     @FXML
     private void searchData(KeyEvent event) {
+        HeroModel s = new HeroModel();
+        String key = search.getText();
+        
+        if (key != "") {
+            ObservableList<HeroModel> data = FXMLMainMenuController.dth.searchItems(key, key, key, key, key, key, key, key, key, key, key, key, key, key);
+            if (data != null) {
+                tbv.getColumns().clear();
+                tbv.getItems().clear();
+                
+                TableColumn col = new TableColumn("Id");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("id"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Name");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("name"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Gender");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("gender"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Description");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("des"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Race Id");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("race_id"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Race name");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("raceName"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Curr class id");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("raceName"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Experience");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("exp"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Curr class name");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("className"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Stat points");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("stat_points"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Skill points");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("skill_points"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("User id");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("user_id"));
+                tbv.getColumns().addAll(col);
+
+                col = new TableColumn("Username");
+                col.setCellValueFactory(new PropertyValueFactory<HeroModel, String>("username"));
+                tbv.getColumns().addAll(col);
+                
+                tbv.setItems(data);
+                tbv.getSelectionModel().selectFirst();
+            } else {
+                Alert a = new Alert(Alert.AlertType.ERROR, "Empty data", ButtonType.OK);
+                a.showAndWait();
+                tbv.getScene().getWindow().hide();;
+            }            
+        } else {
+           showData();
+        }
     }
 
     @FXML
     private void clear1Clicked(ActionEvent event) {
+        search.setText("");
+        txtId.setText("");
+        showData();
+        search.requestFocus();
     }
 
     @FXML
     private void saveClicked(ActionEvent event) {
+        HeroTraitModel n = new HeroTraitModel();
+        n.setHero_id(Integer.parseInt(txtId.getText()));
+        n.setTrait_id(Integer.parseInt(txtId1.getText()));
+        
+        FXMLMainMenuController.dtht.setHeroTraitModel(n);
+        if (FXMLMainMenuController.dtht.validasi(n.getHero_id(), n.getTrait_id()) <= 0) {
+            if (FXMLMainMenuController.dtht.insert()) {
+               Alert a = new Alert(Alert.AlertType.INFORMATION, "Data saved.", ButtonType.OK);
+               a.showAndWait();
+            } else {
+               Alert a = new Alert(Alert.AlertType.ERROR, "Saving data failed.", ButtonType.OK);
+               a.showAndWait();
+            }
+        } else {
+            Alert a = new Alert(Alert.AlertType.ERROR, "There is already the same data.", ButtonType.OK);
+            a.showAndWait();
+        }
     }
 
     @FXML
     private void exitClicked(ActionEvent event) {
+        btnExit.getScene().getWindow().hide();
     }
-    
 }
